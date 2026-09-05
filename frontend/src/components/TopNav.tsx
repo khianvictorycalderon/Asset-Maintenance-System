72da34bd0295bf2b6dd4a5c77b1de600d290d41a
RafaelJPEG <188954702+RafaelJPEG@users.noreply.github.com>
2026-09-05 21:29:56 +0800

    feat: add role-based private pages and maintenance navigation

--- frontend/src/components/TopNav.tsx
+++ frontend/src/components/TopNav.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { BUILT_IN_API_URLS, TOPNAV_HEADER } from "../config";
+import { BUILT_IN_API_URLS, NAVBAR_HEADER } from "../config";
 import { getTheme, setTheme } from "../utils/theme";
 import { clearUser } from "../slices/userSlice";
 import type { RootState } from "../config/store";
@@ -110,7 +110,7 @@ export default function TopNav({ onMenuClick }: Props) {
 
             {/* Title */}
             <div className="ml-2 font-semibold text-neutral-900 dark:text-neutral-100 truncate">
-                {TOPNAV_HEADER}
+                {NAVBAR_HEADER}
             </div>
 
             {/* Right side */}
